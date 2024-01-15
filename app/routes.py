7b7e842edc6e4c48b3d0e3c67243aa1cd7b5c1fe
Anna Tůmová <annie.tumova@gmail.com>
2024-01-15 21:46:28 +0100

    fix requirements, code cleanup, add Rostifile

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,8 @@
-import os
-from dotenv import load_dotenv
-from flask import Flask, render_template, redirect, url_for, flash, request, abort
-from flask_bootstrap import Bootstrap
-from functools import wraps
+from flask import render_template
 from app import app
 
 
 @app.route("/", methods=["GET"], defaults={"path": ""})
 @app.route("/<path:path>", methods=["GET"])
 def index(path):
-    return render_template("/static/build/index.html", active_page=f"/{path}")
+    return render_template("/static/dist/index.html", active_page=f"/{path}")
